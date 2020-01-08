6c969dcdc7e80dda1502040d18f0adfecfda738c
basemEk <basem.kreidly@gmail.com>
2020-01-08 13:04:23 +0200

    logic-5-php

---
+++ part-1/php-logic-solutions/problem5-solution.php
@@ -0,0 +1,13 @@
+<?php
+
+function circleArea ($radius){
+    echo 'Enter radius value';
+    echo 3.14 * $radius * $radius;
+}
+
+$radius = readline('Enter value of radius: ');
+echo "the area of a circle of radius $radius  is = " . 3.14 * $radius * $radius;
+
+
+?>
+
