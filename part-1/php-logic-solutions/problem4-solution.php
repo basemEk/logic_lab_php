45b336ae83e6aa3864a8d766810cd4ade448aad3
basemEk <basem.kreidly@gmail.com>
2020-01-08 12:24:53 +0200

    logic-4-php

---
+++ part-1/php-logic-solutions/problem4-solution.php
@@ -0,0 +1,13 @@
+<?php
+function celciusFehrenheit (){
+    $celcius = readline('Enter your room temprature in degree celcius: ');
+    $fehrenheit;
+    
+    echo 'Your temprature in Fehrenheit is: ' . $fehrenheit = $celcius * (9/5) + 32;
+}
+
+celciusFehrenheit()
+
+?>
+
+
