821be02663b3b54a1db7ae2ee47f51fed9d8f10d
basemEk <basem.kreidly@gmail.com>
2020-01-08 10:37:22 +0200

    logic1-php

--- part-1/php-logic-solutions/problem1-solution.php
+++ part-1/php-logic-solutions/problem1-solution.php
@@ -1,6 +1,15 @@
-$num1;
-$num2;
+<?php
+function checker() {
+$num1 = readline('please enter the first number');
+$num2 = readline('please enter the second number');
 
-if($num1 == $num2){
+if($num1 === $num2){
     echo 'true';
 }
+else{
+echo 'Wrong input';
+exit;
+}
+}
+checker();
+?>
---
+++ part-1/php-logic-solutions/problem2-solution.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1;
+$num2;
+$num3;
+
+if(($num1==$num2) && ($num2==$num3)){
+    echo 'true';
+}
+else{
+    
+}
+
+?>
