53cd93e1fd4dc96e73b5d3ef65f4354ec238992d
basemEk <basem.kreidly@gmail.com>
2020-01-08 22:02:21 +0200

    logic-10-php

---
+++ part-1/php-logic-solutions/problem10-solution.php
@@ -0,0 +1,18 @@
+<?php
+
+function evenOdd (int $num){}
+
+$num = readline('Enter a Number: ');
+
+echo $num % 2 === 0? 'Even' : 'Odd';
+
+// if(is_numeric($num % 2 === 0)){
+//     echo 'Even';
+// }
+// else{
+//     echo 'Odd';
+// exit;
+// }
+
+
+?>
