c30e607a4c7318890ce0c5f62eb09d70eb0868a3
basemEk <basem.kreidly@gmail.com>
2020-01-08 11:44:15 +0200

    problem2-php

--- part-1/php-logic-solutions/problem2-solution.php
+++ part-1/php-logic-solutions/problem2-solution.php
@@ -1,14 +1,32 @@
 <?php
 
-$num1;
-$num2;
-$num3;
+function compareThreeNum(){
+var_export([1,2,3]);
 
-if(($num1==$num2) && ($num2==$num3)){
-    echo 'true';
+$solution_number = '1'; 
+$num1 = readline('Enter the first number: ');
+$num2 = readline('Enter the second number: ');
+$num3 = readline('Enter the third number: ');
+
+switch($solution_number){
+    case '1':
+        var_export(($num1==$num2) && ($num2==$num3));
+    break;
+
+    case '2':
+        var_export(($num3 > $num2) && ($num2 > $num3));
+    break;
+
+    case '3':
+        var_export(($num1==$num2) || ($num2==$num3) || ($num1==$num3));
+    break;
+
+    default:
+			echo 'Wrong solution';
+			break;
 }
-else{
-    
 }
+compareThreeNum();
+
 
 ?>
