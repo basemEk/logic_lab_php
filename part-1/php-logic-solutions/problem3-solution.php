a8ed3967fb220bfb2350ffec519d05198c7c1e37
basemEk <basem.kreidly@gmail.com>
2020-01-08 11:58:07 +0200

    logic-3-php

---
+++ part-1/php-logic-solutions/problem3-solution.php
@@ -0,0 +1,14 @@
+<?php
+
+function firstLastName (){
+    $firstName = readline('Enter your first name: ');
+    $lastName = readline('Enter your last name: ');
+
+    echo 'Hello' .' '. $firstName .' '. $lastName;
+ }
+
+
+ firstLastName();
+
+    
+?>
